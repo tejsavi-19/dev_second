30efdd677f358a7a045950dfc8fdca631d019f13
tejasvi-19 <tejasvipednekar6@gmail.com>
2026-04-26 14:04:15 +0530

    src committed

---
+++ src/test/java/pro1/test.java
@@ -0,0 +1,17 @@
+package pro1;
+import org.testng.Assert;
+
+import org.testng.annotations.Test;
+public class test {
+	public void testPass() {
+		Assert.assertEquals(devops.display(55), "pass");
+		}
+		@Test
+		public void testFail() {
+		Assert.assertEquals(devops.display(35), "fail");
+		}
+		@Test
+		public void testInvalid() {
+		Assert.assertEquals(devops.display(-5), "invalid");
+		}
+}
